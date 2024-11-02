cb8b13d134356a85b7ba6c4c7c48ced4af9a9cfc
Munisankar7781 <munimsd6@gmail.com>
2024-11-02 08:46:34 +0530

    Crud Repository
    crud repo

---
+++ Jpa_CrudRepo-1/src/main/java/com/ms/JpaCrudRepo1Application.java
@@ -0,0 +1,14 @@
+package com.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCrudRepo1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JpaCrudRepo1Application.class, args);
+    }
+
+}
