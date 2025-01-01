863c8f0eeb0c71fa73294d70d7b9cc91ff24134e
Munisankar7781 <munimsd6@gmail.com>
2025-01-01 23:01:06 +0530

    pagination and sorting

---
+++ Pagination and Sortinng/src/main/java/com/ms/PaginationAndSortinngApplication.java
@@ -0,0 +1,13 @@
+package com.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationAndSortinngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaginationAndSortinngApplication.class, args);
+    }
+
+}
